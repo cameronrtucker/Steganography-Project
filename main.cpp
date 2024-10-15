60623bce441d5ccb2886f3594b447c154d3fa489
ellagself <eself6@ut.utm.edu>
2024-10-14 22:07:33 -0500

    main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @author Mangos: Cameron Tucker and Ella Self
+ * @date 2024-10-14
+ * @brief main file for Stenography 
+ * 
+ * stenography main file
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cout << "hi stenography project" << endl; 
+  
+  return 0;
+}
